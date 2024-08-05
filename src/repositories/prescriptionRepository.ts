b5affb1f7a471be73b3fc741f07551ca26418cde
CrisGos <cristianortega9509@gmail.com>
2024-08-04 22:23:42 -0500

    functional

--- src/repositories/prescriptionRepository.ts
+++ src/repositories/prescriptionRepository.ts
@@ -1,6 +1,8 @@
 import { injectable } from 'tsyringe';
 import { Prescription } from '../models/prescription';
 import { Medicine } from '../models/medicine';
+import { createObjectCsvWriter }from 'csv-writer';
+import { resolve, join } from 'path';
 
 @injectable()
 export default class PrescriptionRepository {
@@ -13,14 +15,48 @@ export default class PrescriptionRepository {
     }
 
     async findByPatientId(patientId: number) {
-        return await Prescription.findAll({
+        const data: Prescription[] = await Prescription.findAll({
             where: { patientId },
             include: [Medicine],
         });
+        this.DataAnalysis(data);
+        return data
     }
 
+    async DataAnalysis(data: Prescription[]) {
+        const now = new Date().toISOString().replace(/[:\-T.]/g, '').slice(0, 14);
+        const directory = resolve(__dirname, '../data');
+        const filePath = join(directory, `prescriptions_${now}.csv`);
+        const csv = createObjectCsvWriter({
+            path: filePath,
+            header: [
+                { id: 'id', title: 'ID' },
+                { id: 'dosage', title: 'Dosage' },
+                { id: 'frequency', title: 'Frequency' },
+                { id: 'duration', title: 'Duration' },
+                { id: 'medicineId', title: 'Medicine ID' },
+                { id: 'patientId', title: 'Patient ID' },
+                { id: 'createdAt', title: 'Created At' },
+                { id: 'updatedAt', title: 'Updated At' },
+            ]
+        });
+
+        const info = data.map(prescription => ({
+            id: prescription.id,
+            dosage: prescription.dosage,
+            frequency: prescription.frequency,
+            duration: prescription.duration,
+            medicineId: prescription.medicineId,
+            patientId: prescription.patientId,
+            createdAt: prescription.createdAt,
+            updatedAt: prescription.updatedAt,
+        }));
+
+        await csv.writeRecords(info);
+    };
+
     async create(prescription: Partial<Prescription>) {
-        const medicine = await (Medicine.findByPk(prescription.medicineId));
+        const medicine = await Medicine.findByPk(prescription.medicineId);
         if (!medicine) {
             throw new Error('Medicine not found');
         }
@@ -28,6 +64,7 @@ export default class PrescriptionRepository {
         if (medicine.quantity <= 0) {
             throw new Error('Medicine not available in inventory');
         }
+
 
         return await Prescription.create(prescription);
     }
